71e7a1c406ad7948986928f92c13a9bdaaf602c5
Ajinkya Urgunde <ajinkya.u@easypay.in>
2025-12-11 08:09:59 +0530

    feat(Home.jsx): React DOMs study, feat(aditya.html): DOM actual manupulation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,9 @@ function Home() {
   const { count, increment, data: productsData } = useCounter("https://dummyjson.com/products");
   return (
     <Fragment>
-        <div>Home</div>
+        {/* <div>Home</div> */}
+        <div>Hello world</div>
+        {/* new Virtual DOM || old Virtual DOM -> changes -> Actual DOM*/}
       <h1>count : {count}</h1>
       <button
         onClick={() => {
